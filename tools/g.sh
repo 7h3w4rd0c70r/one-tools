3e7d0b09cb3c6e0df91606015c9cd3129ab12d67
Patrik Šimunič <patriksimunic@hotmail.com>
2018-04-01 04:31:02 +0200

    Init

--- tools/g.sh
+++ tools/g.sh
@@ -16,6 +16,15 @@ case $CMD in
         CURRENT_BRANCH="$(git symbolic-ref --short -q HEAD)"
         git push origin $CURRENT_BRANCH
         ;;
+    "remote")
+        REMOTE_NAME="origin"
+        if git config "remote.$REMOTE_NAME.url" > /dev/null; then
+            git remote set-url $REMOTE_NAME $2
+        else
+            git remote add $REMOTE_NAME $2
+        fi
+        echo "remote $REMOTE_NAME set to $2"
+        ;;
     *)
         echo "Invalid command "$CMD""
         ;;
